ef3ba18e43de9b2871625fa38bffe9da9a67d17a
Harsh Tiwari <131223935+harsh21082004@users.noreply.github.com>
2024-01-09 23:38:56 +0530

    Update navbar.js

--- pages/navbar.js
+++ pages/navbar.js
@@ -37,6 +37,14 @@ const Navbar = () => {
       <nav className={`navbar navbar-expand-lg px-2 ${styles.navbar} ${theme === "light" ? styles.navdark : styles.navlight}`}>
         <div className={`container-fluid`} >
           <Link href="/" className={styles.containerFluid}><span className={theme === "light" ? "textpurpledark" : "textpurplelight"}><b className='fontBold'>&lt;/&gt; Codebyte</b></span></Link>
+          <img
+                className={`${styles.imgMode1} ${theme === "light"?styles.imgdark:styles.imglight}`}
+                src="/nmicon.png"
+                width={40}
+                height={40}
+                alt="Picture of the author"
+                onClick={handleOnClick}
+              />
           <div className={`${styles.hamburger}`} data-bs-toggle="collapse" type="button" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <div
               className={`${theme === "light" ?styles.hamburgerMenu1light : styles.hamburgerMenu1dark} ${open ? '' : styles.open}`}
@@ -47,7 +55,7 @@ const Navbar = () => {
             </div>
           </div>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0 px-3" id='links'>
+            <ul className={`${styles.navul} navbar-nav me-auto mb-2 mb-lg-0 px-3`} id='links'>
               <li className={`nav-item ${styles.navItem}`} >
                 <Link className={`nav-link text-${theme === "light" ? "white" : "black"} `} aria-current="page" href="/" >Home</Link>
               </li>
@@ -68,16 +76,9 @@ const Navbar = () => {
               <input className={`${styles.serBox} form-control me-2`} type="search" placeholder="Search" aria-label="Search" />
               <button className={`btn ${styles.serBtn}`} type="submit">Search</button>
             </form>
-            <Link href={"/signup"}><button className={`btn mx-2 ${styles.serBtn}`} type="submit">SignUp</button></Link>
+            <Link href={"/signup"}><button className={` btn mx-2 ${styles.signBtn}`} type="submit">SignUp</button></Link>
             <span className={`p-1`}>
-              {/* <img
-                className={`${styles.imgMode} ${theme === "light"?styles.imgdark:styles.imglight}`}
-                src="/nmicon.png"
-                width={40}
-                height={40}
-                alt="Picture of the author"
-                onClick={handleOnClick}
-              /> */}
+              
               <img
                 className={`${styles.imgMode} ${theme === "light" ? styles.imgdark : styles.imglight}`}
                 src="/nmicon.png"
