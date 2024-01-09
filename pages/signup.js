21223d14c226ce183078faf188847688f4ef424c
Harsh Tiwari <131223935+harsh21082004@users.noreply.github.com>
2024-01-09 17:18:34 +0530

    Update signup.js

--- pages/signup.js
+++ pages/signup.js
@@ -9,6 +9,8 @@ import { ThemeContext } from './context/themeContext';
 
 const Signup = () => {
 
+    const apiUri = process.env.API_URI;
+
     const { theme, handleOnClick } = useContext(ThemeContext)
 
     const [name, setName] = useState();
@@ -30,7 +32,7 @@ const Signup = () => {
     const handleSubmit = async (e) => {
         e.preventDefault()
         const data = { name, email, password }
-        let res = await fetch('http://localhost:3000/api/signup', {
+        let res = await fetch(apiUri, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
